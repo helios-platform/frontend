3e50ad7e41b78e3955b52bb0f29edd5e536f2902
bspowell <82974206+bspowell@users.noreply.github.com>
2024-07-09 12:34:43 -0700

    feat: added template components for side nav
    Co-authored-by: jamesdrabinsky <jamesdrabinsky@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
+import { Resizable } from './components/Resizable'
+import { Nav } from './components/Nav'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ isCollapsed, setIsCollapsed ] = useState(false)
+  const [ links, setLinks ] = useState([
+    {title: 'SQL Console'},
+    {title: 'Add Data Source'}
+  ])
+  
+  // interface NavProps {
+  //   isCollapsed: boolean
+  //   links: {
+  //     title: string
+  //     label?: string
+  //     icon: LucideIcon
+  //     variant: "default" | "ghost"
+  //   }[]
+  // }
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Nav isCollapsed={isCollapsed} links={links} />
+      {/* <Resizable /> */}
     </>
   )
 }
